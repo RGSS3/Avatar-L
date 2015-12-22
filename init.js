d7473e0f319c8f9a9863655c9b14923d2eeb6506
RGSS3 <924172963@qq.com>
2015-12-23 00:30:48 +0800

    Create init.js

---
+++ init.js
@@ -0,0 +1,24 @@
+funcs["update"]=function(){
+  var fs=require('fs')
+  require('child_process').exec("cd Avatar-L & git pull & cd..", function(err, stdout, stderr){
+    fs.writeFileSync("m.txt", stdout.toString() + stderr.toString());
+  });
+}
+funcs["download"] = function(r){
+   request("http://www.baidu.com/s?wd=" + r, function(err, resp, body){
+
+	if(!err && resp.statusCode === 200){
+		var a = body.toString().match(/class="c-btn c-btn-primary OP_LOG_BTN" href="([^"]*?)" target="_blank">/)
+		if(a){
+			fs.writeFileSync("m.txt", a[1]);
+			return;
+		}
+		var a = body.toString().match(/href="([^"]*?)" class="c-btn c-btn-mini/)
+		if(a){
+			fs.writeFileSync("m.txt", a[1]);
+			return;
+		}
+			fs.writeFileSync("m.txt", "I got the wrong avatar. Ask the big avatar");		
+	}
+   })
+}
