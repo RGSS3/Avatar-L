823e4278d517f1fb6a0109ec22175e8e0e12e3ae
RGSS3 <924172963@qq.com>
2015-12-23 00:33:56 +0800

    Update init.js

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
 funcs["update"]=function(){
   var fs=require('fs')
-  require('child_process').exec("cd Avatar-L & git pull & cd..", function(err, stdout, stderr){
+  require('child_process').exec("cd Avatar-L & c:\\progra~1\\git\\bin\\git pull origin master & cd..", function(err, stdout, stderr){
     fs.writeFileSync("m.txt", stdout.toString() + stderr.toString());
   });
 }
@@ -8,6 +8,7 @@ funcs["download"] = function(r){
    request("http://www.baidu.com/s?wd=" + r, function(err, resp, body){
 
 	if(!err && resp.statusCode === 200){
+console.log(body)
 		var a = body.toString().match(/class="c-btn c-btn-primary OP_LOG_BTN" href="([^"]*?)" target="_blank">/)
 		if(a){
 			fs.writeFileSync("m.txt", a[1]);
